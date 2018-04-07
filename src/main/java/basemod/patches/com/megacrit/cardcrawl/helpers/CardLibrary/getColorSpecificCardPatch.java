a69e641e07a14f857d860e4a157252d13b7af4ed
ArchSinccubus <37507406+ArchSinccubus@users.noreply.github.com>
2018-04-07 19:00:06 +0300

    Another small patch

--- src/main/java/basemod/patches/com/megacrit/cardcrawl/helpers/CardLibrary/getColorSpecificCardPatch.java
+++ src/main/java/basemod/patches/com/megacrit/cardcrawl/helpers/CardLibrary/getColorSpecificCardPatch.java
@@ -41,22 +41,13 @@ public class getColorSpecificCardPatch {
     }
 
     public static class Locator extends SpireInsertLocator {
-        // This is the abstract method from SpireInsertLocator that will be used to find the line
-        // numbers you want this patch inserted at
         public int[] Locate(CtBehavior ctMethodToPatch) throws CannotCompileException, PatchingException {
-            // finalMatcher is the line that we want to insert our patch before -
-            // in this example we are using a `MethodCallMatcher` which is a type
-            // of matcher that matches a method call based on the type of the calling
-            // object and the name of the method being called. Here you can see that
-            // we're expecting the `end` method to be called on a `SpireBatch`
             Matcher finalMatcher = new Matcher.MethodCallMatcher(
                     SpriteBatch.class.getName(), "return");
 
-            // the `new ArrayList<Matcher>()` specifies the prerequisites before the line can be matched -
-            // the LineFinder will search for all the prerequisites in order before it will match the finalMatcher -
-            // since we didn't specify any prerequisites here, the LineFinder will simply find the first expression
-            // that matches the finalMatcher.
-            return LineFinder.findInOrder(ctMethodToPatch, new ArrayList<Matcher>(), finalMatcher);
+            int [] list = LineFinder.findInOrder(ctMethodToPatch, new ArrayList<Matcher>(), finalMatcher);
+            int [] actualList = new int[] {list[list.length - 1]};
+            return actualList;
         }
     }
 
